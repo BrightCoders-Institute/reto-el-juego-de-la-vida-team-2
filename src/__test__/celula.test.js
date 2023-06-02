abf15809672c726d3f8e750917f279ee08947964
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-06-01 19:28:39 -0600

    Agregar test para el archivo tablero.js
    Co-authored-by: Rassiel Mex <RassielMex@users.noreply.github.com>
    Co-authored-by: Miguel Cobian Meraz Co-authored-by: <mcmmiguel@users.noreply.github.com>
    Co-authored-by: IvanCa27 <IvanCa27@users.noreply.github.com>

--- src/__test__/celula.test.js
+++ src/__test__/celula.test.js
@@ -1,42 +1,44 @@
 const { Celula } = require("../Celula.js");
 
 describe("Celula", () => {
-  describe("representacionCelula", () => {
-    test("Ser clase Celula", () => {
-      const celula = new Celula(true);
-      expect(celula).toBeInstanceOf(Celula);
-    });
+  const celula = new Celula();
+  test("Revisa que se realice la instancia de la Clase correctamente", () => {
+    
+    expect(celula).toBeInstanceOf(Celula);
+  });
 
-    test('debería devolver "*" si el estado es true (vivo)', () => {
-      const celulaViva = new Celula();
-      celulaViva.estado = true;
-      expect(celulaViva.representacionCelula()).toBe("*");
+  describe("representacionCelula", () => {
+   
+    test('Debe devolver "*" si el estado es true (vivo)', () => {
+      //const celulaViva = new Celula();
+      celula.estado = true;
+      expect(celula.representacionCelula()).toBe("*");
     });
 
     test('debería devolver "." si el estado es false (muerto)', () => {
-      const celulaMuerta = new Celula();
-      celulaMuerta.estado = false;
-      expect(celulaMuerta.representacionCelula()).toBe(".");
+      //const celulaMuerta = new Celula();
+      celula.estado = false;
+      expect(celula.representacionCelula()).toBe(".");
     });
   });
 
   describe("estadoGen2", () => {
     test("debería devolver false si la célula está viva y tiene menos de 2 vecinos", () => {
-      const celula = new Celula();
+      //const celula = new Celula();
       celula.estado = true;
       celula.vecinos = 1;
       expect(celula.estadoGen2()).toBe(false);
     });
 
     test("debería devolver false si la célula está viva y tiene más de 3 vecinos", () => {
-      const celula = new Celula();
+      //const celula = new Celula();
       celula.estado = true;
       celula.vecinos = 5;
       expect(celula.estadoGen2()).toBe(false);
     });
 
     test("debería devolver true si la célula está viva y tiene 2 o 3 vecinos", () => {
-      const celula = new Celula();
+      //const celula = new Celula();
       celula.estado = true;
       celula.vecinos = 2;
       expect(celula.estadoGen2()).toBe(true);
@@ -46,14 +48,14 @@ describe("Celula", () => {
     });
 
     test("debería devolver true si la célula está muerta y tiene exactamente 3 vecinos vivos", () => {
-      const celula = new Celula();
+      //const celula = new Celula();
       celula.estado = false;
       celula.vecinos = 3;
       expect(celula.estadoGen2()).toBe(true);
     });
 
     test("debería devolver false si la célula está muerta y no tiene exactamente 3 vecinos vivos", () => {
-      const celula = new Celula();
+      //const celula = new Celula();
       celula.estado = false;
       celula.vecinos = 2;
       expect(celula.estadoGen2()).toBe(false);
---
+++ src/__test__/tablero.test.js
@@ -0,0 +1,18 @@
+const { Tablero } = require("../Tablero.js");
+
+describe( "Tablero", () => {
+  const tablero = new Tablero(1,1)
+  test("Revisa que se realice la instancia del Tablero correctamente", () => {  
+    expect(tablero).toBeInstanceOf(Tablero);
+  });
+
+  describe("Gen 1", ()=>{
+
+
+
+  })
+
+  
+
+
+});
