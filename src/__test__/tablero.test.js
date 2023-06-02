f98c17569222d3d67a62d003ed8e4378953fef9a
RassielMex <rhcp94.rm@gmail.com>
2023-06-01 19:45:27 -0600

    TEst Tablero : Filas y columnas gen 1 Co-authored-by: IvanCa27 <IvanCa27@users.noreply.github.com> Co-authored-by: Miguel Cobian Meraz <mcmmiguel@users.noreply.github.com> Co-authored-by: Montserrat Aguilar <montsegv-2@users.noreply.github.com>

--- src/__test__/tablero.test.js
+++ src/__test__/tablero.test.js
@@ -1,18 +1,21 @@
 const { Tablero } = require("../Tablero.js");
 
-describe( "Tablero", () => {
-  const tablero = new Tablero(1,1)
-  test("Revisa que se realice la instancia del Tablero correctamente", () => {  
+describe("Tablero", () => {
+  const tablero = new Tablero(1, 1);
+  test("Revisa que se realice la instancia del Tablero correctamente", () => {
     expect(tablero).toBeInstanceOf(Tablero);
   });
 
-  describe("Gen 1", ()=>{
+  describe("Gen 1", () => {
+    test("Debe tener longitud n filas", () => {
+      const tablero = new Tablero(5, 2);
+      expect(tablero.gen1.length).toBe(tablero.fila);
+    });
 
-
-
-  })
-
-  
-
-
+    test("Cada fila debe tener n columnas", () => {
+      const tablero = new Tablero(2, 4);
+      expect(tablero.gen1[0].length).toBe(tablero.columna);
+    });
+    //test("", () => {});
+  });
 });
